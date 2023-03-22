669bf057d10bcf941641a4663b8b73977ecd05df
wijze <122311198+wijze@users.noreply.github.com>
2023-03-22 21:40:05 +0100

    first commit

---
+++ resources/modules/colorscheme.js
@@ -0,0 +1,66 @@
+const body = document.body
+const root = document.querySelector(':root')
+
+
+
+
+//getting theme that is already set, if not: set it
+var theme = localStorage.getItem('theme');
+
+if(theme==null){
+  theme = {
+    type:"light",
+    custom:{}
+  }
+  localStorage.setItem('theme',JSON.stringify(theme))
+} else{
+  theme = JSON.parse(theme)
+}
+
+
+
+
+function applyCustomColorscheme(newColorScheme){
+  if(( newColorScheme[2].r*299+ newColorScheme[2].g*587+ newColorScheme[2].b*114)/1000  >  70){
+    root.style.setProperty('--text-color', "#000000");
+  } else{
+    root.style.setProperty('--text-color', "#f8f8f8");
+  }
+
+  if(( newColorScheme[0].r*299+ newColorScheme[0].g*587+ newColorScheme[0].b*114)/1000  >  70){
+    root.style.setProperty('--sec-text-color', "#000000");
+  } else{
+    root.style.setProperty('--sec-text-color', "#f8f8f8");
+  }
+
+  for (let i = 0; i < newColorScheme.length; i++) {
+    root.style.setProperty('--color'+(i+1), newColorScheme[i].value);
+  }
+}
+
+
+
+
+async function setRandomTheme(){
+
+  //generating custom color scheme
+  let randomSeed = Math.floor(Math.random()*16777215).toString(16);
+  let newColorScheme = await fetch("https://www.thecolorapi.com/scheme?count=3&hex=" + randomSeed);
+  newColorScheme = await newColorScheme.json();
+  newColorScheme = await newColorScheme.colors.map(function(element){return element.rgb})
+
+  applyCustomColorscheme(newColorScheme)
+
+  //setting for next use
+  body.classList = "random"
+  theme.type = "random"
+  theme.custom = newColorScheme
+  localStorage.setItem("theme", JSON.stringify(theme))
+}
+
+
+function setNormalTheme(newTheme){
+  body.classList = newTheme
+  theme.type = newTheme
+  localStorage.setItem("theme", JSON.stringify(theme))
+}
---
+++ resources/modules/loadnav.js
@@ -0,0 +1,50 @@
+function loadNav(page){
+  let more = false
+  if (page=="html-editor" ||page=="to-do"){more=true}
+  return`
+    <ul class="nav-list">
+      <li class="nav-item">
+        <a href="../../v2/menu.html" class="nav-link">
+          <span class="link-text logo-text">Startup</span>
+          <i class="logo-image fa fa-solid fa-arrow-circle-right"></i>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="${page=='index' ? 'javascript:void(0)' : '../menu/menu.html'}" class="nav-link" ${page=='index' ? 'id="active"':''}>
+          <i class="fa fa-solid fa-home"></i>
+          <span class="link-text">Home</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="${page=='about' ? 'javascript:void(0)' : '../about/about.html'}" class="nav-link" ${page=='about' ? 'id="active"':''}>
+          <i class="fa fa-address-card"></i>
+          <span class="link-text">About</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="${page=='settings' ? 'javascript:void(0)' : '../settings/settings.html'}" class="nav-link" ${page=='settings' ? 'id="active"':''}>
+          <i class="fa fa-solid fa-gear"></i>
+          <span class="link-text">Settings</span>
+        </a>
+      </li>
+
+      <li tabindex="0" class="nav-item has-dropdown nav-link"${more ? 'id="active"':''}>
+        <div>
+          <i class="fa fa-sharp fa-solid fa-list"></i>
+          <span class="link-text">More</span>
+        </div>
+        <ul class="dropdown">
+          <li class='nav-item'>
+            <a ${page=='html-editor' ? 'id="active"':''} href="${page=='html-editor' ? 'javascript:void(0)' : '../html-editor/html-editor.html'} "   class="nav-link">Html-editor  </a>
+          </li>
+          <li class='nav-item'>
+            <a ${page=='to-do' ? 'id="active"':''} href="${page=='to-do' ? 'javascript:void(0)' : '../to-do/to-do.html'} "   class="nav-link">To-do  </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  `
+}
